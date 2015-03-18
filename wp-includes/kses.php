1b47504e3c2f1edb5dd30b5b2c480eb5cb6a85cb
root <root@twofei.com>
2015-03-18 21:43:32 +0800

    disable kses

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -1452,8 +1452,9 @@ function kses_init() {
 		kses_init_filters();
 }
 
-add_action('init', 'kses_init');
-add_action('set_current_user', 'kses_init');
+// 女孩不哭 禁用kses
+//add_action('init', 'kses_init');
+//add_action('set_current_user', 'kses_init');
 
 /**
  * Inline CSS filter
