fa0e3b7de4494c52b6254e6a25f90556ffb869e3
movsb <anhbk@qq.com>
2015-03-25 17:29:36 +0800

    优化IEBar

--- wp-content/themes/twofei/footer.php
+++ wp-content/themes/twofei/footer.php
@@ -2,10 +2,10 @@
 	<footer id="footer" class="mono">
 		<div style="text-align: center;">&copy; <?php echo date('Y'); ?> 女孩不哭 All rights reserved.</div>
 		<div style="text-align: center;">Designed by twofei. Proudly powered by <a target="_blank" style="color: inherit;" href="http://www.wordpress.org">WordPress</a>.</div>
+		<div id="iebar">抱歉，本站无法在“IE”浏览器下很好地显示！</div>
+
 	</footer>
 </div>
-
-<div id="iebar">抱歉，本站无法在“IE”浏览器下很好地显示！</div>
 
 <?php wp_footer(); ?>
 
